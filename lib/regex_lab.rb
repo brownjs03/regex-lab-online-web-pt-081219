6ab26d2d2f93dcda8e948a10e5f55f3dd3bb82e1
Jeffrey Brown <brownjs03@gmail.com>
2019-08-30 03:53:40 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,11 @@
+require 'pry'
+
 def starts_with_a_vowel?(word)
 word.match(/^[aeiouAEIOU]/) ? true : false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-word.scan(/(^[un]|$[ing]/)
+binding.pry 
 end
 
 def words_five_letters_long(text)
