d6857160d770463282556d9c3a8a91c56d7cab98
Jeffrey Brown <brownjs03@gmail.com>
2019-08-30 03:54:26 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,11 +1,9 @@
-require 'pry'
-
 def starts_with_a_vowel?(word)
 word.match(/^[aeiouAEIOU]/) ? true : false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-binding.pry 
+text.scan(/(^[un]|$[ing]/)
 end
 
 def words_five_letters_long(text)
